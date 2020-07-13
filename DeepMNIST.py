a0dd4ded7444373e8d71cbfa6db90bfec8408cfc
Gabriel <gab.zapata@uniandes.edu.co>
2020-07-13 11:47:04 -0500

    General Python Excecises

--- DeepMNIST.py
+++ DeepMNIST.py
@@ -9,7 +9,8 @@ old_v = tf.compat.v1.logging.get_verbosity()
 tf.compat.v1.logging.set_verbosity(tf.logging.ERROR)
 from tensorflow.examples.tutorials.mnist import input_data
 
-
+# Define path to TensorBoard log files
+logPath = './tb_logs/'
 
 # We use tensorflow helper to pull down data from de MNIST site
 # Digits of 28x28 pixels
@@ -20,18 +21,19 @@ mnist = input_data.read_data_sets('MNIST_data/', one_hot=True)
 
 sess = tf.InteractiveSession()
 
-# x is the placeholder for the 28 x 28 image data
+with tf.name_scope('MNIST_Input'):
+    # x is the placeholder for the 28 x 28 image data
+    x = tf.placeholder(tf.float32, shape=[None, 784], name='x')
 
-x = tf.placeholder(tf.float32, shape=[None, 784])
-
-# y_ called y bar and it is a 10 elemento vector, containing the predicted probability of each
-# digit(0, 9) class. Such as [0.14, 0.8, 0, 0, 0, 0, 0, 0, 0, 0.06]
-y_ = tf.placeholder(tf.float32, shape=[None, 10])
+    # y_ called y bar and it is a 10 elemento vector, containing the predicted probability of each
+    # digit(0, 9) class. Such as [0.14, 0.8, 0, 0, 0, 0, 0, 0, 0, 0.06]
+    y_ = tf.placeholder(tf.float32, shape=[None, 10], name='y_')
 
 # Code for Deep Learning
 # Change de MNIST input data from a list of values to a 28 Pixel x 28 Pixel X 1 Gray Scale value Cube
 
-x_image = tf.reshape(x, [-1, 28, 28, 1], name="x_image")
+with tf.name_scope('Input_Reshape'):
+    x_image = tf.reshape(x, [-1, 28, 28, 1], name="x_image")
 
 #RELU -> Returns 0 is the value is less than 0 and the value otherwise y = 0, x < 0 and y = x, x >= 0
 
@@ -39,100 +41,107 @@ x_image = tf.reshape(x, [-1, 28, 28, 1], name="x_image")
 # We are using RELU as the activation function. This must be initialized to a small positive number
 # and with some nouise you don't end up going to zero when comparing diff
 
-def weight_variable(shape):
+def weight_variable(shape, name=None):
     initial = tf.truncated_normal(shape, stddev=0.1)
-    return (tf.Variable(initial))
+    return (tf.Variable(initial, name=name))
 
-def bias_variable(shape):
+def bias_variable(shape, name=None):
     initial = tf.constant(0.1, shape=shape)
-    return (tf.Variable(initial))
+    return (tf.Variable(initial, name=name))
 
 # Convolution and pooling. We do convolution and then pooling to control overfitting
 # Strides indicates how far and in which direction we shift values as we compute new features
 # ksize is the kernel size which is the area we are pooling together
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+def conv2d(x, W, name=None):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME', name=name)
 
-def max_pool_2x2(x):
+def max_pool_2x2(x, name=None):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
+                          strides=[1, 2, 2, 1], padding='SAME', name=name)
 
 
 ## Define Layers in the NN
-
 # 1st Convolution Layer
-# 32 Features per each 5x5 patch of the image
-# 5x5 pixel is the size of the filter
-# 1 is one input channel for gray scale. If we had colors we would have 3 channels (R G B)
-W_conv1  = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
+with tf.name_scope('Conv1'):
 
-# Do convolution of images, add bias and push through RELU Activation
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-# Take results and run through max_pool
-# Recevies the result from all neurons, combine and reduce the dimnesionality of the network and the sensitivity of the network
+    # 32 Features per each 5x5 patch of the image
+    # 5x5 pixel is the size of the filter
+    # 1 is one input channel for gray scale. If we had colors we would have 3 channels (R G B)
+    W_conv1  = weight_variable([5, 5, 1, 32], name='weight')
+    b_conv1 = bias_variable([32], name='bias')
 
-h_pool1 = max_pool_2x2(h_conv1)
+    # Do convolution of images, add bias and push through RELU Activation
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1, name='relu')
+    # Take results and run through max_pool
+    # Recevies the result from all neurons, combine and reduce the dimnesionality of the network and the sensitivity of the network
+
+    h_pool1 = max_pool_2x2(h_conv1, name='pool')
 
 # 2nd Convolution Layer
-# Process the 32 features from convolution layer1 in 5x5 patch. Return 64 features weights and biases
-# 64 Features per each 5x5 patch of the image
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+with tf.name_scope('Conv2'):
+    # Process the 32 features from convolution layer1 in 5x5 patch. Return 64 features weights and biases
+    # 64 Features per each 5x5 patch of the image
+    W_conv2 = weight_variable([5, 5, 32, 64], name='weight')
+    b_conv2 = bias_variable([64], name='bias')
 
-# Do convolution of the output of the 1st convolution layer. Pool results
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-# Reduces the image to a 7x7 Image
-h_pool2 = max_pool_2x2(h_conv2)
+    # Do convolution of the output of the 1st convolution layer. Pool results
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2, name='relu')
+    # Reduces the image to a 7x7 Image
+    h_pool2 = max_pool_2x2(h_conv2, name='pool')
 
 # Fully connected layer
-# We are connecting an image of 7x7 with 64 features to 1024 Neurons
-W_fc1 = weight_variable([7*7*64, 1024])
-b_fc1 = bias_variable([1024])
+with tf.name_scope('FC'):
+    # We are connecting an image of 7x7 with 64 features to 1024 Neurons
+    W_fc1 = weight_variable([7*7*64, 1024], name='weight')
+    b_fc1 = bias_variable([1024], name='bias')
 
-# Connect output of pooling layer 2 as input to full connected layer
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    # Connect output of pooling layer 2 as input to full connected layer
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1, name='relu')
 
-# Dropout some neurons to reduce overfitting
-keep_prob = tf.placeholder(tf.float32) # get dropout probability as a training input
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    # Dropout some neurons to reduce overfitting
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob') # get dropout probability as a training input
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
 # Readout Layer
-# Represents the ten digists 0-9
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
+with tf.name_scope('OUT'):
+    # Represents the ten digists 0-9
+    W_fc2 = weight_variable([1024, 10], name='weight')
+    b_fc2 = bias_variable([10], name='bias')
 
 # Define model
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 # Loss mesurement
-
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y_conv, labels=y_))
+with tf.name_scope('Cross_Entropy'):
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y_conv, labels=y_))
 
 # Loss Optimization
-
-step = tf.train.AdamOptimizer(1e-4).minimize(loss)
+with tf.name_scope('Loss_Optimizer'):
+    step = tf.train.AdamOptimizer(1e-4).minimize(loss)
 
 # What is correct
+with tf.name_scope('Accuracy'):
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    # Review Accuracy of the model
 
-# Review Accuracy of the model
-
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Initialize variables
 
 init = tf.global_variables_initializer()
 sess.run(init)
 
+# TB - Writes the default graph out so we can view its structure
+tbWriter = tf.summary.FileWriter(logPath, sess.graph)
+
 # Train model
 import time
 
 # Define number of steps and and how often to display results
 
-num_steps = 3000
+num_steps = 2000
 display_every = 1000
 
 # Start timer
--- MailingAddress.py
+++ MailingAddress.py
@@ -1,4 +1,2 @@
-print('Gabriel Zapata')
-print('Calle 26 Camino de la Floresta No 9 - 00')
-print('Condominio Astoria - Casa 31')
-print('Chia Colombia')
+print('Joshua')
+print('Calle 900 Camino de la Floresta No 9 - 00')
